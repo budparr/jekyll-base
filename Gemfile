8714f2fa3c97ad220f175866beb8f77c56eb0f9d
budparr <budparr@sonnetmedia.net>
2016-06-02 08:03:36 -0400

    clean house
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- Gemfile
+++ Gemfile
@@ -1,9 +1,8 @@
 source 'https://rubygems.org'
-# gem 'jekyll'
-gem 'github-pages'
+ gem 'jekyll'
+#gem 'github-pages'
 
 group :jekyll_plugins do
-  gem "html-proofer"
   gem 'jekyll-seo-tag'
   gem 'jekyll-sitemap'
 end
--- _app/gulp/tasks/file-copy.js
+++
@@ -1,7 +0,0 @@
-cp = require('child_process');
-  module.exports = function(gulp, plugins, config) {
-    gulp.task('file-copy', function() {
-    return gulp.src(config.filesSrc)
-      .pipe(plugins.copy(config.filesSrcDest, {prefix: 3}));
-      });
-  };
