c36bd618b4dab08dace5cad1eff917287ec69081
budparr <budparr@sonnetmedia.net>
2016-06-02 16:59:49 -0400

    add ESLint and do some tests
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+  $('#js-navigation-menu').removeClass('show');
 
   menuToggle.on('click', function(e) {
     e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
+    $('#js-navigation-menu').slideToggle(function() {
+      if ($('#js-navigation-menu').is(':hidden')) {
         $('#js-navigation-menu').removeAttr('style');
       }
     });
@@ -14,12 +14,13 @@ $(document).ready(function() {
 
 var nav = document.querySelector('.js-menu-trigger');
 
-var toggleState = function (elem, one, two) {
-  var elem = document.querySelector(elem);
-  elem.setAttribute('data-state', elem.getAttribute('data-state') === one ? two : one);
+var toggleState = function(elem, one, two) {
+  // var elem = document.querySelector();
+  elem.setAttribute('data-state',
+    elem.getAttribute('data-state') === one ? two : one);
 };
 
-nav.onclick = function (e) {
+nav.onclick = function(e) {
   toggleState('.sliding-panel', 'closed', 'open');
   e.preventDefault();
 };
